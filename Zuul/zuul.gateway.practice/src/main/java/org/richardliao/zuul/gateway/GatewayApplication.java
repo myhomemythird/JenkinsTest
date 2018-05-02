b5b5d2c9e0e75185e5f293a1c1c7a3f6b81ab1ff
Richard <myhomemythird@163.com>
2018-05-02 19:05:17 +0800

    Zuul pratcie

---
+++ Zuul/zuul.gateway.practice/src/main/java/org/richardliao/zuul/gateway/GatewayApplication.java
@@ -0,0 +1,22 @@
+package org.richardliao.zuul.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.richardliao.zuul.gateway.filters.pre.SimpleFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+
+  @Bean
+  public SimpleFilter simpleFilter() {
+    return new SimpleFilter();
+  }
+
+}
