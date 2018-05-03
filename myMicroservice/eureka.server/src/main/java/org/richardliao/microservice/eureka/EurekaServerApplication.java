b2e5542535287c86f030d1310c362f0b7d5a2c83
Richard <myhomemythird@163.com>
2018-05-03 21:59:08 +0800

    Microservice practice

---
+++ myMicroservice/eureka.server/src/main/java/org/richardliao/microservice/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package org.richardliao.microservice.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
