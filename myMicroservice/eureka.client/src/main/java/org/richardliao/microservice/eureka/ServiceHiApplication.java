d8836df4162c4169fc8f1d1d504ddf878ef6eb81
Richard <myhomemythird@163.com>
2018-05-11 22:20:03 +0800

    Sync up changes

--- myMicroservice/eureka.client/src/main/java/org/richardliao/microservice/eureka/ServiceHiApplication.java
+++ myMicroservice/eureka.client/src/main/java/org/richardliao/microservice/eureka/ServiceHiApplication.java
@@ -3,8 +3,12 @@ package org.richardliao.microservice.eureka;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,10 +23,29 @@ public class ServiceHiApplication {
 
     @Value("${server.port}")
     String port;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+    
     @RequestMapping("/hi")
     public String home(@RequestParam String name) {
         return "hi "+name+",i am from port:" +port;
     }
+    
+    @RequestMapping("/hi-trace")
+    public String hiTrace(@RequestParam String name) {
+        System.out.println("[Calling Trace] hiTrace()");
+        return restTemplate.getForObject("http://localhost:8089/callme?name=" + name, String.class);
+    }
+    
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
+    }
 
 }
-
